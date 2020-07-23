77a98debe0d8399d2277f5a54c1f4714eea3c7f9
MozinaTadeja <64742598+MozinaTadeja@users.noreply.github.com>
2020-07-23 16:42:54 +0200

    Dodan tekstovni vmesnik in spremembe v modelu

--- model.py
+++ model.py
@@ -1,9 +1,14 @@
 import random
+import json
+
 
 
 ZE_ZASEDENO = "Z"
 NAPACEN_ZNAK = "#"
-NAPACNA_STEVILKA = "~"
+NAPACEN_UGIB = "&"
+KONEC = "E"
+NADALJUJ = "C"
+TRI = "T"
 seznam = [1,2,3,4,5,6,7,8,9]
 
 
@@ -17,21 +22,64 @@ def transponiraj(matrika): #transponira sudoku v obliki matrike
     return transponiranka
 
 def preveri_sudoku(sudoku): #preveri, če je sudoku rešen pravilno
-    for vrstica in sudoku:
+    for vrstica in sudoku: #ali je v vsaki vrstici vseh 9 stevilk
         i = 1
         while i <= len(sudoku):
             if i not in vrstica:
                 return False
             i += 1
     transponiran = transponiraj(sudoku)
-    for vrstica in transponiran:
+    for vrstica in transponiran: #ali je v vsakem stolpcu vseh 9 stevilk
         i = 1
         while i <= len(transponiran):
             if i not in vrstica:
                 return False
             i += 1
+    seznam = [0,3,6]
+    for j in seznam: #preveri ali je v vsakem kvadratku 3×3 vseh 9 stevilk
+        li = []
+        for vrstica in sudoku:
+            if vrstica == sudoku[j] or vrstica == sudoku[j+1] or vrstica == sudoku[j+2]:
+                l = vrstica[j:j+3]
+                for m in l:
+                    li.append(m)
+        i = 1
+        while i <= 9:
+            if i not in li:
+                return False
+            i += 1
     return True
-    
+
+def preveri_delno(sudoku):
+    for vrstica in sudoku:
+        seznam_stevilk = []
+        for element in vrstica:
+            if element != 0:
+                seznam_stevilk.append(element)
+        for i in seznam_stevilk:
+            if seznam_stevilk.count(i) > 1:
+                return False
+    for vrstica in transponiraj(sudoku):
+        seznam_stevilk = []
+        for element in vrstica:
+            if element != 0:
+                seznam_stevilk.append(element)
+        for i in seznam_stevilk:
+            if seznam_stevilk.count(i) > 1:
+                return False
+    seznam3 = [0,3,6]
+    for j in seznam3: 
+        li = []
+        for vrstica in sudoku:
+            if vrstica == sudoku[j] or vrstica == sudoku[j+1] or vrstica == sudoku[j+2]:
+                l = vrstica[j:j+3]
+                for m in l:
+                    if m != 0:
+                        li.append(m)
+        for i in li:
+            if li.count(i) > 1:
+                return False
+    return True
 
 class Igra:
 
@@ -42,37 +90,60 @@ class Igra:
         else:
             self.ugibi = ugibi
 
-    def pravilni_del(self): #izpise do sedaj resen del
-        mreza = self.plosca
+    def pravilni_del(self): #izpiše do sedaj rešen del
+        plosca = self.plosca
+        mreza = json.loads(plosca)
         for j in self.ugibi:
-            vrstica = j[0][0]
-            stolpec = j[0][1]
-            stevilka = j[1]            
+            vrstica = j[0]
+            stolpec = j[1]
+            stevilka = j[2]            
             mreza[vrstica - 1][stolpec - 1] = stevilka
         return mreza
+
+    def napisana_polja(self, seznami):
+        niz = ""
+        for vrstica in seznami:
+            prazno = ""
+            for mesto in vrstica:
+                prazno = prazno + " " + str(mesto)
+            prazno += " \n"
+            niz += prazno
+        return niz
 
     def konec(self): #sudoku je rešen
         return preveri_sudoku(self.pravilni_del())
 
     def ugibaj(self, ugib):
-        vrstica = ugib[0][0]
-        stolpec = ugib[0][1]
-        stevilka = ugib[1]
+        vrstica = ugib[0]
+        stolpec = ugib[1]
+        stevilka = ugib[2]
+        if len(ugib) != 3:
+            return TRI
         if vrstica not in seznam and stolpec not in seznam and stevilka not in seznam: 
             return NAPACEN_ZNAK    #preveri, da so vsi vpisani znaki stevilke med 1 in 9
-        if self.plosca[vrstica - 1][stolpec - 1] != 0:
+        plosca = json.loads(self.plosca)
+        if plosca[vrstica - 1][stolpec - 1] != 0:
             return ZE_ZASEDENO #če je na zacetni plosci na tem mestu stevilka, je sem ne mores vpisati
-        for i in self.ugibi: #če je pozicija novega ugiba enaka kateremu izmed prejsnjih, prejsnjega zamenja z novim
-            if i[0][0] == vrstica and i[0][1] == stolpec:
+        for i in self.ugibi: #če je mesto novega ugiba enaka kateremu izmed prejsnjih, prejsnjega zamenja z novim
+            if i[0] == vrstica and i[1] == stolpec:
                 self.ugibi.replace(i, ugib)
         else:
             self.ugibi.append(ugib)
+            if preveri_delno(self.pravilni_del()) == False: #sproti preveri, če ugib lahko pride na to mesto
+                self.ugibi = self.ugibi[:-1]
+                return NAPACEN_UGIB 
+            if self.konec():
+                return KONEC
+            else:
+                return NADALJUJ
 
 
 with open("Plosce.txt", "r", encoding="utf-8") as datoteka_z_ploscami:
     mozne_plosce = [vrstica for vrstica in datoteka_z_ploscami]
 
 
-def nova_igra():
+def nova_igra(): 
     return Igra(random.choice(mozne_plosce))
+
+
 
---
+++ tekstovni-vmesnik.py
@@ -0,0 +1,57 @@
+import model
+
+
+def izpis_igre(igra):
+    tekst = (
+        "###############################\n\n"
+        "{pravilni_del}\n\n"
+        "###############################"
+    ).format(
+        pravilni_del=igra.napisana_polja(igra.pravilni_del())   
+    )
+    return tekst
+
+def izpis_konca(igra):
+    tekst = "\n ##### Bravo! Sudoku je končan! #####"
+    return tekst
+
+def izpis_neenako():
+    return "\n\n ##### Vpisati moraš tri številke! #####"
+def izpis_napacni_znak():
+    return "\n\n ##### Ugib naj vsebuje samo številke! #####"
+
+def izpis_zasedeno():
+    return "\n\n ##### Na to mesto ne moreš vstaviti številke. #####"
+
+def izpis_napacni_ugib():
+    return "\n\n ##### Še malo razmisli. #####"
+
+def zahtevaj_vnos():
+    return input("Mesto in številka:  ")
+
+def pozeni_vmesnik():
+
+    igra = model.nova_igra()
+
+    while True:
+        print(izpis_igre(igra))
+        poskus = zahtevaj_vnos()
+        poskus_v_seznamih = [int(i) for i in poskus.split(",")]
+        rezultat_ugiba = igra.ugibaj(poskus_v_seznamih)
+        if rezultat_ugiba == model.TRI:
+            print()
+        if rezultat_ugiba == model.NAPACEN_ZNAK:
+            print(izpis_napacni_znak())
+        elif rezultat_ugiba == model.ZE_ZASEDENO:
+            print(izpis_zasedeno())
+        elif rezultat_ugiba == model.NAPACEN_UGIB:
+            print(izpis_napacni_ugib())
+        elif rezultat_ugiba == model.KONEC:
+            print(izpis_konca(igra))
+            ponovni_zagon = input("Za ponovni zagon vpišite R.").strip()
+            if ponovni_zagon == "R":
+                igra = model.nova_igra()
+            else:
+                break
+
+pozeni_vmesnik()
